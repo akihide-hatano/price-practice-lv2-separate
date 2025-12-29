cb1cfe38b655d88d3e4c265d86afdc32ff429fd9
akihide-hatano <a.h.19841202@gmail.com>
2025-12-30 08:38:02 +0900

    AppTest,PriceCalculatorをマイナス値は受け付けないように修正し、それに対応するテストケースを追加しました。

--- src/main/java/com/discount/PriceCalculator.java
+++ src/main/java/com/discount/PriceCalculator.java
@@ -3,6 +3,9 @@ package com.discount;
 public class PriceCalculator {
 
     public double calculate(int price,boolean isPremium){
+        if( price < 0 ){
+            throw new IllegalArgumentException("price must be >= 0");
+        }
         if(isPremium ){
             return price * 0.9;
         }
--- src/test/java/com/discount/AppTest.java
+++ src/test/java/com/discount/AppTest.java
@@ -3,8 +3,33 @@ package com.discount;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.beans.Transient;
-
 class AppTest {
 
+    @Test
+    void プレミアム会員なら割引される(){
+        PriceCalculator calculator = new PriceCalculator();
+        double result = calculator.calculate(1000, true);
+        assertEquals(900, result);
+    }
+
+    @Test
+    void 通常会員なら割引されない(){
+        PriceCalculator calculator = new PriceCalculator();
+        double result = calculator.calculate(1000, false);
+        assertEquals(1000, result);
+    }
+
+    @Test
+    void 価格が0ならプレミアム会員でも0円のまま(){
+        PriceCalculator calculator = new PriceCalculator();
+        double result = calculator.calculate(0, true);
+        assertEquals(0, result);
+    }
+
+    @Test
+    void マイナス値の入力でも計算さえれてしまう(){
+        PriceCalculator calculator = new PriceCalculator();
+        double result = calculator.calculate(-1000, false);
+        assertEquals(-1000,result);
+    }
 }
