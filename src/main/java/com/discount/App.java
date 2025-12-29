e389673c23d8da8372ea58fefb4d14961b20bc93
akihide-hatano <a.h.19841202@gmail.com>
2025-12-29 20:21:59 +0900

    app.javaを修正。

--- src/main/java/com/discount/App.java
+++ src/main/java/com/discount/App.java
@@ -4,10 +4,9 @@ package com.discount;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args){
+        System.out.println("=== 割引アプリ (Lv2 / 分離版) ===");
     }
 }
