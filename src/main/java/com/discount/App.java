91b9e11b4cc76c42a2d58d3cee89f212c570a062
akihide-hatano <a.h.19841202@gmail.com>
2025-12-29 20:51:14 +0900

    app.javaを更新

--- src/main/java/com/discount/App.java
+++ src/main/java/com/discount/App.java
@@ -1,5 +1,7 @@
 package com.discount;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,5 +10,23 @@ public class App
 {
     public static void main(String[] args){
         System.out.println("=== 割引アプリ (Lv2 / 分離版) ===");
+
+        //計算ロジック担当クラスを作成
+        PriceCalculator calculator = new PriceCalculator();
+
+        try(Scanner scanner = new Scanner(System.in)){
+
+            //1.価格入力
+            System.out.println("商品価格を入力してください > ");
+            String priceInput = scanner.nextLine().trim();
+
+            int price;
+
+            try {
+                price = Integer.parseInt(priceInput);
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }
     }
 }
